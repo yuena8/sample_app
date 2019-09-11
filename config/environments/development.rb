22998f58c99dff66f52cbdf5f5af4ae918e800c1
yuena8 <amy.yuen8@gmail.com>
2019-09-11 04:23:17 +0000

    Added Hello World

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  # Allow connections to local server.
+  config.hosts.clear
 
   # Do not eager load code on boot.
   config.eager_load = false
