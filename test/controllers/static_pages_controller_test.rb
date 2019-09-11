52645f2add8b8a36bc3a2a70ae38c474a3815c1e
yuena8 <amy.yuen8@gmail.com>
2019-09-11 19:39:55 +0000

    Finished staticc pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,42 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  # setup function is run before every test
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  
+  # test gets a URL and verifies/asserts that the result is a success
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    get static_pages_home_url # issues GET request to home url
+    assert_response :success  # receive a 'success' status code in response
+    assert_select "title", "Home | #{@base_title}" # checks the presense of HTML tag
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-  end
+    assert_select "title", "Help | #{@base_title}"
 
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
 end
