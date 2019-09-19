9293d6fbeae610f4ed4f282c41cc5f352df90e62
yuena8 <amy.yuen8@gmail.com>
2019-09-19 16:09:10 +0000

    Added user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home' # sets application's root route
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
