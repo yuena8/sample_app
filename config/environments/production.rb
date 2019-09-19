c567b4d6108a70c9c40b334032155124d30120a1
yuena8 <amy.yuen8@gmail.com>
2019-09-19 00:49:34 +0000

    Added user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
